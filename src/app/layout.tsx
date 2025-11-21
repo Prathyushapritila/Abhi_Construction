82756fd6af64fd1f6147995327a1c190d5bdb925
Vamsi Krishna Kosuri <vamsikrishnakosuri@Vamsis-MacBook-Air.local>
2025-11-20 21:38:46 -0600

    Remove: Global logo watermark completely

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -86,7 +86,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <div className="flex min-h-screen flex-col relative page-wrapper">
+          <div className="flex min-h-screen flex-col relative">
             <div className="relative z-10">
               <Navbar />
               <main className="flex-1">{children}</main>
